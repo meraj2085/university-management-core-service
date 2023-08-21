4d430d202e253e717402d5ec4e5b4d6750f48be5
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-08-21 10:18:20 +0600

    academic semester completed

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -2,14 +2,32 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
 import { AcademicSemesterValidation } from './academicSemester.validation';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 const router = express.Router();
 
 
 router.get('/', AcademicSemesterController.getAllFromDB)
+
+router.get('/:id', AcademicSemesterController.getDataById)
+
 router.post(
   '/',
   validateRequest(AcademicSemesterValidation.create),
   AcademicSemesterController.insertIntoDB
+);
+
+router.patch(
+  '/:id',
+  validateRequest(AcademicSemesterValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.deleteByIdFromDB
 );
 
 export const AcademicSemesterRoutes = router;
