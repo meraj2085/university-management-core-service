f6e384423fb0a68714bfcc3200cbd953f8715642
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-08-21 09:35:04 +0600

    Adding getFromDb

---
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -0,0 +1,81 @@
+import { AcademicSemester, Prisma, PrismaClient } from '@prisma/client';
+import { IAcademicSemesterFilterRequest } from './academicSemester.interface';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import { IGenericResponse } from '../../../interfaces/common';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { AcademicSemesterSearchAbleFields } from './academicSemester.constants';
+
+const prisma = new PrismaClient();
+
+const insertIntoDB = async (
+  data: AcademicSemester
+): Promise<AcademicSemester> => {
+  const result = await prisma.academicSemester.create({
+    data,
+  });
+  return result;
+};
+
+const getAllFromDB = async (
+  filters: IAcademicSemesterFilterRequest,
+  options: IPaginationOptions
+): Promise<IGenericResponse<AcademicSemester[]>> => {
+  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+  console.log(options);
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: AcademicSemesterSearchAbleFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AcademicSemesterWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.academicSemester.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: 'desc',
+          },
+  });
+
+  const total = await prisma.academicSemester.count();
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
+export const AcademicSemesterService = {
+  insertIntoDB,
+  getAllFromDB
+};
