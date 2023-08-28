4eafad5b863f99d2d786d2f918dc4d0cf60464ca
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-08-28 18:12:57 +0600

    M-37

--- src/app/modules/faculty/faculty.routes.ts
+++ src/app/modules/faculty/faculty.routes.ts
@@ -2,33 +2,45 @@ import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
-import { FacultyController } from './faculty.controller';
-import { FacultyValidation } from './faculty.validations';
+import { CourseController } from '../course/course.controller';
+import { CourseValidation } from '../course/course.validations';
 
 const router = express.Router();
-
-router.get('/', FacultyController.getAllFromDB);
-
-router.get('/:id', FacultyController.getByIdFromDB);
-
+router.get('/', CourseController.getAllFromDB);
+router.get('/:id', CourseController.getByIdFromDB);
 
 router.post(
-    '/',
-    validateRequest(FacultyValidation.create),
-    FacultyController.insertIntoDB
+  '/',
+  validateRequest(CourseValidation.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  CourseController.insertIntoDB
 );
 
 router.patch(
-    '/:id',
-    validateRequest(FacultyValidation.update),
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    FacultyController.updateOneInDB
+  '/:id',
+  validateRequest(CourseValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  CourseController.updateOneInDB
 );
 
 router.delete(
-    '/:id',
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    FacultyController.deleteByIdFromDB
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  CourseController.deleteByIdFromDB
+);
+
+router.post(
+  '/:id/assign-faculties',
+  validateRequest(CourseValidation.assignOrRemoveFaculties),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  CourseController.assignFaculties
+);
+
+router.delete(
+  '/:id/remove-faculties',
+  validateRequest(CourseValidation.assignOrRemoveFaculties),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  CourseController.removeFaculties
 );
 
 export const facultyRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
 import { academicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
-import { facultyRoutes } from '../modules/faculty/faculty.routes';
 import { studentRoutes } from '../modules/student/student.routes';
 import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 import { buildingRoutes } from '../modules/building/building.routes';
 import { roomRoutes } from '../modules/room/room.routes';
 import { courseRoutes } from '../modules/course/course.routes';
+import { facultyRoutes } from '../modules/faculty/faculty.routes';
+import { semesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.routes';
 
 const router = express.Router();
 
@@ -33,16 +34,20 @@ const moduleRoutes = [
   },
   {
     path: '/buildings',
-    route: buildingRoutes
+    route: buildingRoutes,
   },
   {
     path: '/rooms',
-    route: roomRoutes
+    route: roomRoutes,
   },
   {
     path: '/courses',
-    route: courseRoutes
-  }
+    route: courseRoutes,
+  },
+  {
+    path: '/semester-registration',
+    route: semesterRegistrationRoutes,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
